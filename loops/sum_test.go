cb3ff59777984e80e5393555fb75fb3b40272f13
Teerapong Ladlee <blckpearl.sheeper@gmail.com>
2022-10-11 09:58:43 +0700

    test: add loops SumOfFirst test with failed case

---
+++ loops/sum_test.go
@@ -0,0 +1,28 @@
+package loops
+
+import "testing"
+
+type testCase struct {
+	arg1 int
+	want int
+}
+
+func SumOfFirst(n int) int {
+	return 0
+}
+
+func TestSumOfFirst(t *testing.T) {
+	cases := []testCase{
+		{0, 0},
+		{1, 1},
+		{2, 3},
+		{3, 6},
+	}
+
+	for _, tc := range cases {
+		got := SumOfFirst(tc.arg1)
+		if tc.want != got {
+			t.Errorf("Expected %d, but got %d", tc.want, got)
+		}
+	}
+}
